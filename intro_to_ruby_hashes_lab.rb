4b06a749635be671e4bd549033a766a1aa565850
David Lee <david.lee.sl2634@yale.edu>
2020-05-14 20:44:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,10 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  transportation = {:railroads => {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {:reading_railroad => "morgage_value" = "100", :pennsylvania_railroad => "mortgage_value" = "200", :b_and_o_railroad => "mortgage_value" = "400", :shortline_railroad => "mortgage_value" = "800"}
+  transportation = {:railroads => {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {:reading_railroad => {"morgage_value" = "100"}, :pennsylvania_railroad => {"mortgage_value" = "200"}, :b_and_o_railroad => {"mortgage_value" = "400"}, :shortline_railroad => {"mortgage_value" = "800"}
     }
   }
-
+}
 end
 
 
